63e123c4d14aee119d40cb7c4c5da5429fa32396
Mikhail Efremov <jakutenshi@gmail.com>
2021-02-21 23:13:54 +0300

    Add students to the scripts

--- fetch.sh
+++ fetch.sh
@@ -1,13 +1,12 @@
-git fetch TheLifes08       # Колованов
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
-#git fetch                 # 
+git fetch TheLifes08      # Колованов 
+git fetch dkamakin        # Камакин 
+git fetch moskalenko9381  # Москаленко 
+git fetch Sashauxgit      # Семенов 
+git fetch shakhinn        # Шахин 
+git fetch Andrexus23      # Матвеев 
+git fetch Auhadiev        # Аухадиев 
+git fetch Irina8888       # Андрух 
+git fetch PtichkinSergey  # Птичкин 
 #git fetch                 # 
 #git fetch                 # 
 #git fetch                 # 
--- remotes.sh
+++ remotes.sh
@@ -1,12 +1,12 @@
 git remote add TheLifes08                https://github.com/TheLifes08/spbetu_os_2021_9381.git         # Колованов
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
-#git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
+git remote add dkamakin                  https://github.com/dkamakin/spbetu_os_2021_9381.git           # Камакин
+git remote add moskalenko9381            https://github.com/moskalenko9381/spbetu_os_2021_9381.git     # Москаленко
+git remote add Sashauxgit                https://github.com/Sashauxgit/spbetu_os_2021_9381.git         # Семенов
+git remote add shakhinn                  https://github.com/shakhinn/spbetu_os_2021_9381.git           # Шахин
+git remote add Andrexus23                https://github.com/Andrexus23/spbetu_os_2021_9381.git         # Матвеев
+git remote add Auhadiev                  https://github.com/Auhadiev/spbetu_os_2021_9381.git           # Аухадиев
+git remote add Irina8888                 https://github.com/Irina8888/spbetu_os_2021_9381.git          # Андрух
+git remote add PtichkinSergey            https://github.com/PtichkinSergey/spbetu_os_2021_9381.git     # Птичкин
 #git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
 #git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
 #git remote add                           https://github.com//spbetu_os_2021_9381.git                  # 
